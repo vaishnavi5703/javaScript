c50e0cb25644dca5ae9320f4282002107f78e0be
vaishnavi5703 <122341054+vaishnavi5703@users.noreply.github.com>
2023-12-11 05:21:10 +0000

    conversion

---
+++ basics/conversionOperator.js
@@ -0,0 +1,62 @@
+let score="0033";
+console.log(typeof score);
+let valueInNumber =Number(score)
+console.log(typeof valueInNumber); 
+console.log(valueInNumber); // 33
+
+// let a="33ab";
+// console.log(typeof a);
+// let InNumber =Number(a)
+// console.log(typeof InNumber);  
+// console.log(InNumber); //NaN
+
+
+// let b=null;
+// console.log(typeof b);
+// let InNo =Number(b)
+// console.log(typeof InNo);  
+// console.log(InNo);  //0
+
+// let c;
+// console.log(typeof c); //Undefined
+// let No =Number(c)
+// console.log(typeof No);  
+// console.log(No);  //Nan
+
+//integer conversion
+/*
+"33"=>33
+"33abc"=>NaN
+true=>1; false=>0
+*/
+
+
+// let a;
+// let b=Boolean(a)
+// console.log(b);
+
+//boolean conversion
+/*
+1=>true
+"vaishu"=>true
+0  null  undefined "" =>false
+*/
+
+
+//string conversion
+// let a=null;
+// let b=String(a)
+// console.log(b);
+// console.log(typeof b);
+
+
+
+
+//****************operations*****************************
+
+// let value=5
+// let negvalue=-value
+// console.log(value);
+// console.log(negvalue);
+
+//2**3  ==>2^3
