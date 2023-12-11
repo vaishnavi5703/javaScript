1246449aa0fb624df478a81a62efd69c7ca8d35c
vaishnavi5703 <122341054+vaishnavi5703@users.noreply.github.com>
2023-12-11 04:31:19 +0000

    datatype

---
+++ basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as newer version
+// alert("hello")  //we are using nodejs,not browser
+//code readability should be high 
+
+let name="vaishu"
+let age=18
+let isloggedIn=false
+
+/*
+number=>2^53
+bigint
+string=> ""
+boolean =>true/false
+null =>standalone value
+undefined=>not assign value
+symbol =>unique
+*/
+console.log(typeof name);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
---
+++ basics/variables.js
@@ -0,0 +1,15 @@
+const prn=21610008
+let Fname="vaishnavi Vitthal Patil"
+var password="1234"
+email="patilvaishu@gmail.com"
+let villege;
+// prn=2    can't do like this..bcoz we declare prn as const
+Fname="sanika Patil"
+password="5678"
+email="patilsanika@gmail.com"
+console.log(prn);
+console.table([prn,Fname,password,email,villege])   // printt data in form of table
+/*
+prefer not to use var
+bcoz of issue in block scope and functinal scope
+*/
